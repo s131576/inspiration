573284360445a0dcb65c9f0b7ff283b476acc673
s131576 <s131576@ap.be>
2024-07-19 08:38:10 +0200

    niewe versie

--- shop/app/api/users/route.ts
+++ shop/app/api/users/route.ts
@@ -1,10 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { connectToDatabase } from "@/helpers/server-helpers";
 
 export async function GET(request: NextRequest) {
   try {
-    await connectToDatabase();
     const users = await prisma.user.findMany();
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
@@ -24,18 +22,20 @@ export async function POST(request: NextRequest) {
     if (!name || !email) {
       return NextResponse.json({ message: "Invalid Request" }, { status: 400 });
     }
-    await connectToDatabase();
+
     const user = await prisma.user.findUnique({
       where: {
         email,
       },
     });
+
     if (user) {
       return NextResponse.json(
         { message: "User already exists" },
         { status: 400 }
       );
     }
+
     // create new user
     const newUser = await prisma.user.create({
       data: {
